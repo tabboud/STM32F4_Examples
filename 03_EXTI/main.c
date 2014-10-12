46f6c55209a44fb1bb5dc05dc84ef5061bb449a7
Tony Abboud <tony.abboud54@gmail.com>
2014-10-12 19:00:12 -0400

    Direct reg. access lines

--- 03_EXTI/main.c
+++ 03_EXTI/main.c
@@ -54,16 +54,17 @@ void initialize() {
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
     GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;		// PA0 is connected to high, so use pulldown resistor
     GPIO_Init(GPIOA, &GPIO_InitStructure);
     
-    /* Connect EXTI Line0 to PA0 pin */
+    /* Connect EXTI Line0 to PA0 pin (i.e. EXTI0CR[0])*/
     SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
+    // SYSCFG->EXTICR[0] &= SYSCFG_EXTICR1_EXTI0_PA;		// Same as above, but with direct register access
     
     /* Configure EXTI Line0 */
     EXTI_InitStructure.EXTI_Line = EXTI_Line0;              // PA0 is connected to EXTI0
     EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;     // Interrupt mode
-    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling; // Trigger on Rising edge
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising; 	// Trigger on Rising edge (Just as user presses btn)
     EXTI_InitStructure.EXTI_LineCmd = ENABLE;               // Enable the interrupt
     EXTI_Init(&EXTI_InitStructure);                         // Initialize EXTI
     
@@ -75,6 +76,16 @@ void initialize() {
     NVIC_Init(&NVIC_InitStructure);                                 // Add to NVIC
 }
 
+
+void EXTI0_IRQHandler(void) {
+    // Make sure the interrupt flag is set for EXTI0
+    if(EXTI_GetITStatus(EXTI_Line0) != RESET){
+        GPIO_ToggleBits(GPIOD, GPIO_Pin_13);
+        
+        // Clear the interrupt flag
+        EXTI_ClearITPendingBit(EXTI_Line0);
+    }
+}
 
 int main(void) {
     setSysTick();
